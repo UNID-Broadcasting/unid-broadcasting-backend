c761cfa33ae52d22ed9a7d6295ba0f00c5cd2e3e
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-11-22 13:49:59 -0600

    Se agrega función de edición
    Falta desarrollar el código para editar un podcast

--- src/controllers/podcast.controller.js
+++ src/controllers/podcast.controller.js
@@ -1,36 +1,36 @@
 const Podcast = require("../models/podcast");
 
 const getPodcasts = async (req, res) => {
-    try {
-        const podcasts = await Podcast.find();
-        res.status(200).json(podcasts);
-    }
-    catch (error) {
-        res.status(500).json({
-            message: "Error al obtener los podcasts",
-            error
-        });
-    }
-}
+  try {
+    const podcasts = await Podcast.find();
+    res.status(200).json(podcasts);
+  } catch (error) {
+    res.status(500).json({
+      message: "Error al obtener los podcasts",
+      error,
+    });
+  }
+};
 
 const createPodcast = async (req, res) => {
-    try {
-        const podcast = new Podcast(req.body);
-        await podcast.save();
-        res.status(200).json({
-            message: "Podcast creado correctamente",
-            podcast
-        });
-    }
-    catch (error) {
-        res.status(500).json({
-            message: "Error al crear el podcast",
-            error
-        });
-    }
-}
+  try {
+    const podcast = new Podcast(req.body);
+    await podcast.save();
+    res.status(200).json({
+      message: "Podcast creado correctamente",
+      podcast,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error al crear el podcast",
+      error,
+    });
+  }
+};
+
+const editPodcast = async (req, res) => {};
 
 module.exports = {
-    getPodcasts,
-    createPodcast
-}
+  getPodcasts,
+  createPodcast,
+};
