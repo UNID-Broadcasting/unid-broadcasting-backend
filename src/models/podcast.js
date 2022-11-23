26b0c2994628a4aa73943d43f63820fcaaa6f295
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-11-22 19:21:07 -0600

    URL se añade al esquema de podcast

--- src/models/podcast.js
+++ src/models/podcast.js
@@ -1,19 +1,20 @@
 const { Schema, model } = require("mongoose");
 
 const podcastSchema = new Schema({
-    name: String,
-    description: String,
-    podcastNumber: Number,
-    image: String,
-    date: Date,
-    duration: Number,
-    category: String
+  name: String,
+  description: String,
+  podcastNumber: Number,
+  image: String,
+  date: Date,
+  duration: Number,
+  category: String,
+  url: String,
 });
 
 podcastSchema.methods.toJSON = function () {
-    const { __v, _id, ...podcast } = this.toObject();
-    podcast.uid = _id;
-    return podcast;
+  const { __v, _id, ...podcast } = this.toObject();
+  podcast.uid = _id;
+  return podcast;
 };
 
 const Podcast = model("Podcast", podcastSchema);
