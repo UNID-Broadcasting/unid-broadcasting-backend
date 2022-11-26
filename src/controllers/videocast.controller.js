fb32de18dc9590da4538d68561016b7d2e5e48b1
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-11-25 22:14:38 -0600

    Update and Delete functions added

--- src/controllers/videocast.controller.js
+++ src/controllers/videocast.controller.js
@@ -1,36 +1,69 @@
 const VideoCast = require("../models/videocast");
 
 const getVideoCasts = async (req, res) => {
-    try {
-        const videoCasts = await VideoCast.find();
-        res.status(200).json(videoCasts);
-    }
-    catch (error) {
-        res.status(500).json({
-            message: "Error al obtener los videoCasts",
-            error
-        });
-    }
-}
+  try {
+    const videoCasts = await VideoCast.find();
+    res.status(200).json(videoCasts);
+  } catch (error) {
+    res.status(500).json({
+      message: "Error al obtener los videoCasts",
+      error,
+    });
+  }
+};
 
 const createVideoCast = async (req, res) => {
-    try {
-        const videoCast = new VideoCast(req.body);
-        await videoCast.save();
-        res.status(200).json({
-            message: "VideoCast creado correctamente",
-            videoCast
-        });
-    }
-    catch (error) {
-        res.status(500).json({
-            message: "Error al crear el videoCast",
-            error
-        });
-    }
-}
+  try {
+    const videoCast = new VideoCast(req.body);
+    await videoCast.save();
+    res.status(200).json({
+      message: "VideoCast creado correctamente",
+      videoCast,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error al crear el videoCast",
+      error,
+    });
+  }
+};
+
+const updateVideoCast = async (req, res) => {
+  try {
+    const videoCast = await VideoCast.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json({
+      message: "VideoCast actualizado correctamente",
+      videoCast,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error al actualizar el videoCast",
+      error,
+    });
+  }
+};
+
+const deleteVideoCast = async (req, res) => {
+  try {
+    await VideoCast.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+      message: "VideoCast eliminado correctamente",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error al eliminar el videoCast",
+      error,
+    });
+  }
+};
 
 module.exports = {
-    getVideoCasts,
-    createVideoCast
-}
+  getVideoCasts,
+  createVideoCast,
+  updateVideoCast,
+  deleteVideoCast,
+};
--- src/routes/videocast.routes.js
+++ src/routes/videocast.routes.js
@@ -1,9 +1,16 @@
 const { Router } = require("express");
-const {getVideoCasts, createVideoCast} = require("../controllers/videocast.controller")
+const {
+  getVideoCasts,
+  createVideoCast,
+  updateVideoCast,
+  deleteVideoCast,
+} = require("../controllers/videocast.controller");
 
 const router = Router();
 
 router.get("/", getVideoCasts);
 router.post("/", createVideoCast);
+router.put("/:id", updateVideoCast);
+router.delete("/:id", deleteVideoCast);
 
 module.exports = router;
