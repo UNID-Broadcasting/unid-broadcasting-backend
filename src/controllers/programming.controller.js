dceb97d54aab12d7b7a7a877526fe29ccea95f03
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-11-25 21:51:42 -0600

    Update and Delete Functions added

--- src/controllers/programming.controller.js
+++ src/controllers/programming.controller.js
@@ -1,36 +1,69 @@
 const Programming = require("../models/programming");
 
 const getProgramming = async (req, res) => {
-    try {
-        const programming = await Programming.find();
-        res.status(200).json(programming);
-    }
-    catch (error) {
-        res.status(500).json({
-            message: "Error al obtener la programación",
-            error
-        });
-    }
-}
+  try {
+    const programming = await Programming.find();
+    res.status(200).json(programming);
+  } catch (error) {
+    res.status(500).json({
+      message: "Error al obtener la programación",
+      error,
+    });
+  }
+};
 
 const createProgramming = async (req, res) => {
-    try {
-        const programming = new Programming(req.body);
-        await programming.save();
-        res.status(200).json({
-            message: "Programación creada correctamente",
-            programming
-        });
-    }
-    catch (error) {
-        res.status(500).json({
-            message: "Error al crear la programación",
-            error
-        });
-    }
-}
+  try {
+    const programming = new Programming(req.body);
+    await programming.save();
+    res.status(200).json({
+      message: "Programación creada correctamente",
+      programming,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error al crear la programación",
+      error,
+    });
+  }
+};
+
+const updateProgramming = async (req, res) => {
+  try {
+    const programming = await Programming.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json({
+      message: "Programación actualizada correctamente",
+      programming,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error al actualizar la programación",
+      error,
+    });
+  }
+};
+
+const deleteProgramming = async (req, res) => {
+  try {
+    await Programming.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+      message: "Programación eliminada correctamente",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error al eliminar la programación",
+      error,
+    });
+  }
+};
 
 module.exports = {
-    getProgramming,
-    createProgramming
-}
+  getProgramming,
+  createProgramming,
+  updateProgramming,
+  deleteProgramming,
+};
--- src/routes/programming.routes.js
+++ src/routes/programming.routes.js
@@ -1,9 +1,16 @@
 const { Router } = require("express");
-const {getProgramming, createProgramming} = require("../controllers/programming.controller")
+const {
+  getProgramming,
+  createProgramming,
+  updateProgramming,
+  deleteProgramming,
+} = require("../controllers/programming.controller");
 
 const router = Router();
 
 router.get("/", getProgramming);
 router.post("/", createProgramming);
+router.put("/:id", updateProgramming);
+router.delete("/:id", deleteProgramming);
 
 module.exports = router;
