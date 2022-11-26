b2ac22c90f880afc98e4fcf6b11d0c4d9f910a6b
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-11-25 22:07:41 -0600

    Update and Delete function added

--- src/controllers/podcast.controller.js
+++ src/controllers/podcast.controller.js
@@ -1,36 +1,67 @@
 const Podcast = require("../models/podcast");
 
 const getPodcasts = async (req, res) => {
-    try {
-        const podcasts = await Podcast.find();
-        res.status(200).json(podcasts);
-    }
-    catch (error) {
-        res.status(500).json({
-            message: "Error al obtener los podcasts",
-            error
-        });
-    }
-}
+  try {
+    const podcasts = await Podcast.find();
+    res.status(200).json(podcasts);
+  } catch (error) {
+    res.status(500).json({
+      message: "Error al obtener los podcasts",
+      error,
+    });
+  }
+};
 
 const createPodcast = async (req, res) => {
-    try {
-        const podcast = new Podcast(req.body);
-        await podcast.save();
-        res.status(200).json({
-            message: "Podcast creado correctamente",
-            podcast
-        });
-    }
-    catch (error) {
-        res.status(500).json({
-            message: "Error al crear el podcast",
-            error
-        });
-    }
-}
+  try {
+    const podcast = new Podcast(req.body);
+    await podcast.save();
+    res.status(200).json({
+      message: "Podcast creado correctamente",
+      podcast,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error al crear el podcast",
+      error,
+    });
+  }
+};
+
+const updatePodcast = async (req, res) => {
+  try {
+    const podcast = await Podcast.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(200).json({
+      message: "Podcast actualizado correctamente",
+      podcast,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error al actualizar el podcast",
+      error,
+    });
+  }
+};
+
+const deletePodcast = async (req, res) => {
+  try {
+    await Podcast.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+      message: "Podcast eliminado correctamente",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error al eliminar el podcast",
+      error,
+    });
+  }
+};
 
 module.exports = {
-    getPodcasts,
-    createPodcast
-}
+  getPodcasts,
+  createPodcast,
+  updatePodcast,
+  deletePodcast,
+};
--- src/routes/podcast.routes.js
+++ src/routes/podcast.routes.js
@@ -1,9 +1,16 @@
 const { Router } = require("express");
-const {getPodcasts, createPodcast} = require("../controllers/podcast.controller")
+const {
+  getPodcasts,
+  createPodcast,
+  updatePodcast,
+  deletePodcast,
+} = require("../controllers/podcast.controller");
 
 const router = Router();
 
 router.get("/", getPodcasts);
 router.post("/", createPodcast);
+router.put("/:id", updatePodcast);
+router.delete("/:id", deletePodcast);
 
 module.exports = router;
