c580ac6da1064b37d30e40c96a472264bcd57439
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-11-29 20:47:21 -0600

    Language changed to "es" and first character uppercased

--- src/controllers/date.js
+++ src/controllers/date.js
@@ -1,18 +1,20 @@
-/* Vamos a declarar en una variable la zona horaria y apartir de allí vamos a recuperar el nombre del día, día, nombre del mes, año y hora */
 const moment = require("moment-timezone");
 const timezone = "America/Mexico_City";
 
-const dayName = moment().tz(timezone).format("dddd");
+const dayName = moment().tz(timezone).locale("es").format("dddd");
 const day = moment().tz(timezone).format("DD");
-const monthName = moment().tz(timezone).format("MMMM");
+const monthName = moment().tz(timezone).locale("es").format("MMMM");
 const year = moment().tz(timezone).format("YYYY");
 const time = moment().tz(timezone).format("HH:mm:ss");
 
+const dayNameUpper = dayName.charAt(0).toUpperCase() + dayName.slice(1);
+const monthNameUpper = monthName.charAt(0).toUpperCase() + monthName.slice(1);
+
 const today = (req, res) => {
   res.json({
-    dayName,
+    dayName: dayNameUpper,
     day,
-    monthName,
+    monthName: monthNameUpper,
     year,
     time,
   });
